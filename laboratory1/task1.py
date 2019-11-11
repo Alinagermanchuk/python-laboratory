5e1423909399766b29efd63f6bdf604db7240214
Alina Germanchuk <al5663312@gmail.com>
2019-11-11 23:02:37 +0300

    Додано валідатор

--- laboratory1/task1.py
+++ laboratory1/task1.py
@@ -1,9 +1,11 @@
+from validators import *
+
 print("Германчук Аліна Вікторівна \nЛабораторна робота №1 \nВаріант 7 \nЗнаходження суми вкладу\n")
-a=float(input("Введіть початковий вклад "))
-b=float(input("Введіть відсоток "))
-s=a*(1+b/100)**5
-if a<0 or b<0:
+deposit = float(input("Введіть початковий вклад: "))
+persent = float(input("Введіть відсоток "))
+sum = deposit*(1 + persent/100)**5
+if deposit < 0 or persent < 0:
     answer = "Вклад або відсоток не може бути від`ємним"
 else:
-    answer = "Сума вкладу через 5 років=" + str(s)
+    answer = "Сума вкладу через 5 років = " + str(round(sum,2))
 print(answer)
--- laboratory1/task2.py
+++ laboratory1/task2.py
@@ -1,18 +1,20 @@
+from validators import *
+
 print("Германчук Аліна Вікторівна \nЛабораторна робота №1 \nВаріант 7 \nЗнаходження кількості негатвних чисел\n")
-a=float(input("Введіть число a "))
-b=float(input("Введіть число b "))
-c=float(input("Введіть число c "))
-if a<0:
-        x=1
+a = float(input("Введіть число a: "))
+b = float(input("Введіть число b: "))
+c = float(input("Введіть число c: "))
+if a < 0:
+        x = 1
 else:
-        x=0
+        x = 0
 if b<0:
-        y=1
+        y = 1
 else:
-        y=0
-if c<0:
-        z=1
+        y = 0
+if c < 0 :
+        z = 1
 else:
-        z=0
-s=x+y+z
+        z = 0
+s = x+y+z
 print("Кількість негативних чисел -", str(s))
