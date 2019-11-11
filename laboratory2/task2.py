f8cca1d1e3887deadb6a6c36b855f4739b509480
Alina Germanchuk <al5663312@gmail.com>
2019-11-11 21:03:21 +0300

    Додано 2 лабораторну

---
+++ laboratory2/task2.py
@@ -0,0 +1,7 @@
+print("Германчук Аліна Вікторівна \nЛабораторна робота №2 \nВаріант 7 \nОбчислення добутку (x+i)\n")
+n=int(input('Введіть n: '))
+x=int(input('Введіть x: '))
+product = x+1
+for i in range(2, n+1):
+        product *= (x+i)
+print('Результат добутку (x+i) = ',product)
